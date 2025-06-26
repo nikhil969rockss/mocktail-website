6ff681da2588dd92d6f85386d4851fb9cff65e5b
nikhil969rockss <ns9013604867@gmail.com>
2025-06-26 09:38:33 +0530

    navbar section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+
+//GSAP plugins
 gsap.registerPlugin(ScrollTrigger, SplitText);
+
 const App = () => {
-  return <div className="flex-center h-[100vh]">App</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 export default App;
