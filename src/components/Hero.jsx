ea980f2926c987e32686fc6f68a3f69520330d34
nikhil969rockss <ns9013604867@gmail.com>
2025-06-26 22:23:05 +0530

    title change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,7 +67,7 @@ const Hero = () => {
   return (
     <>
       <section id="hero" className="noisy">
-        <h1 className="title">mojito</h1>
+        <h1 className="title uppercase">mojito</h1>
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
